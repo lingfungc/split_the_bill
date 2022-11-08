88e587f73e451b42478f2a5070c0650dabd757f0
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-08 17:43:13 +0000

    completed splitTheBill function with Object.keys() && forEach() functions

--- split_the_bill.js
+++ split_the_bill.js
@@ -1,3 +1,19 @@
 const splitTheBill = (group) => {
-
+  let bill = 0;
+  Object.keys(group).forEach((key) => {
+    bill += group[key];
+  });
+  bill /= Object.keys(group).length;
+  Object.keys(group).forEach((key) => {
+    group[key] -= bill;
+  });
+  return group;
 };
+
+// const group = {
+//   "john"  : 120,
+//   "paul"  :  30,
+//   "ringo" : 150,
+// }
+
+// console.log(splitTheBill(group));
